728e1e1ff52bf4f12e5f8949eb4777447c8927a7
JoaoFonseca197 <joao.hc.fonseca197@gmail.com>
2020-02-27 17:52:27 +0000

    Strings forms

--- LP1Aula1/VariusStrings/Program.cs
+++ LP1Aula1/VariusStrings/Program.cs
@@ -6,10 +6,18 @@ namespace VariusStrings
     {
         static void Main(string[] args)
         {
+            int x = 420;
+            int y = 69;
             string comida = ("Pão com queijo");
             string acucar = (@""" acucar""");
+            string smoke = ("smoke" + "weed" + "at" + x);
+            string best_numbers = ($"best numbers are {x} and {y}");
+            string Dead = ($@"""Omewa Mo {x}ero""");
             Console.WriteLine(comida);
             Console.WriteLine(acucar);
+            Console.WriteLine(smoke);
+            Console.WriteLine(best_number);
+            Console.WriteLine(Dead);
         }
     }
 }
