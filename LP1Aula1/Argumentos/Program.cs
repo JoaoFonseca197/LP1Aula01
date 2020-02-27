929a52077df99adf74b8009bb6763c300e4bcf9d
JoaoFonseca197 <joao.hc.fonseca197@gmail.com>
2020-02-27 16:16:38 +0000

    Added comments

--- LP1Aula1/Argumentos/Program.cs
+++ LP1Aula1/Argumentos/Program.cs
@@ -7,8 +7,9 @@ namespace Argumentos
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            for(int i = 0; i< args.Length; i++)
+            for (int i = 0; i < args.Length; i++)
             {
+
                 Console.WriteLine(args[i]);
             }
         }
