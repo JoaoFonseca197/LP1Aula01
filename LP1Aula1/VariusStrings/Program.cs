33ce842526d59d87b1d467d0754a116bf265f466
JoaoFonseca197 <joao.hc.fonseca197@gmail.com>
2020-02-27 18:15:19 +0000

    Strings forms XD

--- LP1Aula1/VariusStrings/Program.cs
+++ LP1Aula1/VariusStrings/Program.cs
@@ -13,11 +13,19 @@ namespace VariusStrings
             string smoke = ("smoke " + "weed " + "at" + x);
             string best_numbers = ($"best numbers are {x} and {y}");
             string Dead = ($@"""Omewa Mo {x}ero""");
+            double xx = 0.12345;
+            int ii = 18;
             Console.WriteLine(comida);
             Console.WriteLine(acucar);
             Console.WriteLine(smoke);
             Console.WriteLine(best_numbers);
             Console.WriteLine(Dead);
+            Console.WriteLine($"x={xx:f2}");
+            Console.WriteLine($"x={xx:p1}");
+            Console.WriteLine($"x={ii:x}");
+            Console.WriteLine($"x={ii:c}");
+
+
         }
     }
 }
