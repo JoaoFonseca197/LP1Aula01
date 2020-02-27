d2078dfe980b0b8e134a14a13f8908977c0e62c5
JoaoFonseca197 <joao.hc.fonseca197@gmail.com>
2020-02-27 17:15:09 +0000

    added floats

--- LP1Aula1/TiposVarios/Program.cs
+++ LP1Aula1/TiposVarios/Program.cs
@@ -10,6 +10,8 @@ namespace TiposVarios
             num1 = 3;
             num2 = 4;
             char Iene =\u00A5;
+            float = 3.666f;
+            float = 3.555f;
             char circle 0 = \u058D;
             Console.WriteLine("Hello World!");
         }
