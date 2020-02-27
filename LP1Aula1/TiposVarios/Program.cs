c8088c3d3156392d26a661a00bd9708cf6b6eb2a
JoaoFonseca197 <joao.hc.fonseca197@gmail.com>
2020-02-27 17:23:39 +0000

    BUGS FIXED

--- LP1Aula1/TiposVarios/Program.cs
+++ LP1Aula1/TiposVarios/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace TiposVarios
 {
@@ -6,14 +7,20 @@ namespace TiposVarios
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
             int num1 ,num2;
             num1 = 3;
             num2 = 4;
-            char Iene =\u00A5;
-            float = 3.666f;
-            float = 3.555f;
-            char circle 0 = \u058D;
-            Console.WriteLine("Hello World!");
+            char Iene ='\u00A5';
+            float numfloat1 = 3.666f;
+            float numfloat2= 3.555f;
+            char Circle  = '\u058D';
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            Console.WriteLine(Iene);
+            Console.WriteLine(Circle);
+            Console.WriteLine(numfloat1);
+            Console.WriteLine(numfloat2);
         }
     }
 }
