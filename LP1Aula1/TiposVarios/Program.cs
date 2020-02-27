541e59fbb215142dfb0ae5b703f338acf66394ca
JoaoFonseca197 <joao.hc.fonseca197@gmail.com>
2020-02-27 17:11:36 +0000

    added Variables

--- LP1Aula1/TiposVarios/Program.cs
+++ LP1Aula1/TiposVarios/Program.cs
@@ -6,6 +6,11 @@ namespace TiposVarios
     {
         static void Main(string[] args)
         {
+            int num1 ,num2;
+            num1 = 3;
+            num2 = 4;
+            char Iene =\u00A5;
+            char circle 0 = \u058D;
             Console.WriteLine("Hello World!");
         }
     }
