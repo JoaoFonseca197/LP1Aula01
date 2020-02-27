f9ed63849e49eb541bfe917866959f18f00a7cdb
JoaoFonseca197 <joao.hc.fonseca197@gmail.com>
2020-02-27 17:37:38 +0000

    Strings

---
+++ LP1Aula1/VariusStrings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VariusStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comida = ("Pão com queijo");
+            string acucar = (@""" acucar""");
+            Console.WriteLine(comida);
+            Console.WriteLine(acucar);
+        }
+    }
+}
